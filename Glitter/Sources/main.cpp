b71ff675c7aec2e7b5e4f757f05bfda9faa0c160
Carwyn Nelson <carwyn.nelson@bbc.co.uk>
2018-08-14 11:16:35 +0100

    little bit of refactoring

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -24,95 +24,66 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
 
-int CompileShader(GLenum shaderType, const char* shaderSource)
-{
-    int shaderId = glCreateShader(shaderType);
-    glShaderSource(shaderId, 1, &shaderSource, NULL);
-    glCompileShader(shaderId);
-
-    int success;
-    char infoLog[512];
-    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shaderId, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    return shaderId;
-}
-
-int LinkShaderProgram(int shader1, int shader2)
-{
-    int success;
-    char infoLog[512];
-
-    int shaderProgram = glCreateProgram();
-
-    glAttachShader(shaderProgram, shader1);
-    glAttachShader(shaderProgram, shader2);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
-    glDeleteShader(shader1);
-    glDeleteShader(shader2);
-
-    return shaderProgram;
-}
-
-int LoadModel(std::vector<float> vertices, std::vector<unsigned int> indices)
-{
-	// generate our buffers
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	// bind a vertexArrayObject that we can save our data to
-    glBindVertexArray(VAO);
-
-	// bind the array buffer and save the vertices data to it
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * 4, vertices.data(), GL_STATIC_DRAW);
-
-	// bind the element array buffer and save the indices to it
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-
-	// describe the layout of our vertex data to the shader program
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-	// unbind the vertexArrayObject
-    glBindVertexArray(0); 
-
-    return VAO;
-}
-
 class ShaderProgram
 {
 	const char* mVertexShaderSource;
 	const char* mFragmentShaderSource;
-	int mShaderId;
+	int mProgramId;
 public:
 	ShaderProgram(const char* vertexSource, const char* fragmentSource)
 		: mVertexShaderSource(vertexSource)
 		, mFragmentShaderSource(fragmentSource)
 	{
-		int vertexShader = CompileShader(GL_VERTEX_SHADER, mVertexShaderSource);
-		int fragmentShader = CompileShader(GL_FRAGMENT_SHADER, mFragmentShaderSource);
-		mShaderId = LinkShaderProgram(vertexShader, fragmentShader);
+		int vertexShader = Compile(GL_VERTEX_SHADER, mVertexShaderSource);
+		int fragmentShader = Compile(GL_FRAGMENT_SHADER, mFragmentShaderSource);
+		mProgramId = Link(vertexShader, fragmentShader);
 	}
 
 	void Use() const
 	{
-		glUseProgram(mShaderId);
+		glUseProgram(mProgramId);
 	}
+private:
+    int Compile(GLenum shaderType, const char* shaderSource)
+    {
+        int shaderId = glCreateShader(shaderType);
+        glShaderSource(shaderId, 1, &shaderSource, NULL);
+        glCompileShader(shaderId);
+
+        int success;
+        char infoLog[512];
+        glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shaderId, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        return shaderId;
+    }
+
+    int Link(int shader1, int shader2)
+    {
+        int success;
+        char infoLog[512];
+
+        int shaderProgram = glCreateProgram();
+
+        glAttachShader(shaderProgram, shader1);
+        glAttachShader(shaderProgram, shader2);
+        glLinkProgram(shaderProgram);
+
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+
+        glDeleteShader(shader1);
+        glDeleteShader(shader2);
+
+        return shaderProgram;
+    }
 };
 
 class Model
@@ -120,7 +91,7 @@ class Model
 public:
 	Model(const ShaderProgram& program) : mProgram(program)
 	{
-		mModelVao = LoadModel(mVertices, mIndices);
+		mModelVao = Load();
 	}
 	
 	void Render()
@@ -131,6 +102,35 @@ public:
 		glBindVertexArray(0);
 	}
 private:
+    int Load()
+    {
+        // generate our buffers
+        unsigned int VBO, VAO, EBO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        // bind a vertexArrayObject that we can save our data to
+        glBindVertexArray(VAO);
+
+        // bind the array buffer and save the vertices data to it
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, mVertices.size() * 4, mVertices.data(), GL_STATIC_DRAW);
+
+        // bind the element array buffer and save the indices to it
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, mIndices.size() * sizeof(unsigned int), mIndices.data(), GL_STATIC_DRAW);
+
+        // describe the layout of our vertex data to the shader program
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // unbind the vertexArrayObject
+        glBindVertexArray(0); 
+
+        return VAO;
+    }
+
 	int mModelVao;
 	const ShaderProgram& mProgram;
 
